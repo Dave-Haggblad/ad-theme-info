c2c142101c60c11e3031898bc53593d1836e3a45
Dave-Haggblad <dave.haggblad@antennagroup.com>
2022-05-31 12:58:46 -0700

    Update ad-theme-info.php

--- ad-theme-info.php
+++ ad-theme-info.php
@@ -3,7 +3,7 @@
 Plugin Name: Antenna Digital Theme Info
 Plugin URI: https://www.antennagroup.com
 Description: Displays the template files while viewing page as admin. Dave's version.
-Version: 1.1
+Version: 1.2
 Author: Antenna | Digital
 Author URI: https://wwww.antennagroup.com
 License: GPLv2 or later
